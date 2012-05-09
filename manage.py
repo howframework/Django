d53a4246e5b45ee59746fb347c50ae308e15ca0a
Kamal Bin Mustafa <kamal@smach.net>
2012-05-10 03:23:34 +0800

    renamed openshift to djangohow

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(PROJECT_ROOT, 'app'))
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openshift.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangohow.settings")
 
     from django.core.management import execute_from_command_line
 
